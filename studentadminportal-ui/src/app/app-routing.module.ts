cafcef745e7876be60e85f9341833f396bc5e3ce
jaison10 <jaisonj1010@gmail.com>
2023-07-12 23:45:30 +0530

    route

--- studentadminportal-ui/src/app/app-routing.module.ts
+++ studentadminportal-ui/src/app/app-routing.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router'
+import { StudentsComponent } from './students/students.component';
 
-const routes: Routes = []
+const routes: Routes = [
+  {
+    path:'',
+    component : StudentsComponent
+  },
+  {
+    path:'ha',
+    component : StudentsComponent
+  }
+]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes) //this is how routes are defined
+  ],
+  exports: [    //exporting the routes
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
