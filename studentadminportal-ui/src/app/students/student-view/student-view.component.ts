9ccafc335067e633f704bc63c3431be0d471c4bd
jaison10 <jaisonj1010@gmail.com>
2023-08-21 22:24:09 +0530

    displaying fname

--- studentadminportal-ui/src/app/students/student-view/student-view.component.ts
+++ studentadminportal-ui/src/app/students/student-view/student-view.component.ts
@@ -18,20 +18,27 @@ export class StudentViewComponent implements OnInit {
 
   private studentIdFrmURL : string | null | undefined
   email = new FormControl('', [Validators.required, Validators.email]);
+  student : Student | undefined;
+  studentFName : String | undefined 
 
   constructor(private StudentService : StudentsService, private readonly routes : ActivatedRoute) {  }
 
   ngOnInit(): void {
     //this is for reading value from the URL - params
-    // this.routes.paramMap.subscribe((params)=>{
-    //   this.studentIdFrmURL = params.get("Id"); //this "Id" shud be same as the one given in app.routing for the route provided.
-    // })
+    this.routes.paramMap.subscribe((params)=>{
+      this.studentIdFrmURL = params.get("Id"); //this "Id" shud be same as the one given in app.routing for the route provided.
+    })
 
     if(this.studentIdFrmURL){
       console.log("The given ID is: ", this.studentIdFrmURL);
       //if some value is given in route, the below function in Services will be called passing ID.
-      this.StudentService.getStudentDet(this.studentIdFrmURL).subscribe((student : Student)=>{
-        console.log(student);
+      this.StudentService.getStudentDet(this.studentIdFrmURL).subscribe((studentData : Student)=>{
+        console.log("Data from DB" , studentData);
+        this.student = studentData
+        console.log("Saved Data", this.student);
+        this.studentFName = this.student?.firstname
+        console.log("Student FName", this.studentFName);
+      
       },
       (error)=>{ 
         //the backend is expecting a GUID. If the given value isnt a GUID or if the given val is GUID and curresponding student isnt found, both cases will return an error.
